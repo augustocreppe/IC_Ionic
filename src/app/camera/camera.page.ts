5e05eab408089bf5eb3205fbb6b2d2f2475a95fc
augustocreppe <augusto.creppe@gmail.com>
2021-01-26 14:04:32 -0300

    a

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.page.html',
   styleUrls: ['./camera.page.scss'],
 })
+
 export class CameraPage implements OnInit {
 
-  constructor() { }
+  photo: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl))
+  }
 
   ngOnInit() {
   }
