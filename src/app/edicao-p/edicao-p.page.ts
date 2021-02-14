ab0e88becd4edcd67d0bfdeab1abd1f2034de5ff
augustocreppe <augusto.creppe@gmail.com>
2021-02-14 14:10:22 -0300

    consistencia arrumada

--- src/app/edicao-p/edicao-p.page.ts
+++ src/app/edicao-p/edicao-p.page.ts
@@ -27,10 +27,10 @@ export class EdicaoPPage implements OnInit {
     this.formEditP2 = this.formBuilder.group({
       nome:   ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
       data:   [this.defaultDate],
-      cpf:    ['', [Validators.required, Validators.minLength(14), Validators.maxLength(14), Validators.pattern('^[0-9]+$')]],
-      cep:    ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^[0-9]+$')]],
+      cpf:    ['', [Validators.required, Validators.minLength(14), Validators.maxLength(14)]],
+      cep:    ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       email:  ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      tel:    ['', [Validators.required, Validators.minLength(15), Validators.maxLength(15), Validators.pattern('^[0-9]+$')]],
+      tel:    ['', [Validators.required, Validators.minLength(15), Validators.maxLength(15)]],
       tipo:   ['', [Validators.required]]
     })
   }
