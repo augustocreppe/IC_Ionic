9cb241c2160264414e739d42a459f0e3f31b741f
augustocreppe <augusto.creppe@gmail.com>
2021-06-09 09:26:59 -0300

    Edição Home + Conta

--- src/app/conta/conta.page.ts
+++ src/app/conta/conta.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ContaPage implements OnInit {
-  constructor() { }
-  ngOnInit() { }
+  cidade: string
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.pesquisarPlaca('EUL2F71').subscribe((res) => {
+      const data = res.body;
+      console.log(data);
+    });
+  }
+
+  pesquisarPlaca(placa: string) {
+    const url = `https://apicarros.com/v1/consulta/${placa}/json`;
+
+    return this.http.get(url, {
+      observe: 'response'
+    });
+  }
 }
